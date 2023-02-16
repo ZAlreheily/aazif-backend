747ce6c2cd3553e8279202a8f1a5b02819ba3320
Ziad Alreheily <ziyad1913@gmail.com>
2023-02-16 21:51:03 +0300

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+
+
+
+app.use(notFoundError);
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+    console.log(`Server is Running on Port ${PORT}`)
+})
